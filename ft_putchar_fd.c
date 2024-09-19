c865acffd61a0603f8d39eda01d34d9bac543e39
nataiuca <movil.nataiuca@gmail.com>
2024-09-19 18:54:26 +0200

    Add files via upload

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -10,8 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 void	ft_putchar_fd(char c, int fd)
 {
-
+    write(fd, &c, 1);
+}
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -10,8 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
 {
+   int  i;
 
+   i = 0;
+   if (fd >= 0 && s)
+   {
+    while (s[i] != '\0')
+        write(fd, &s[i], 1);
+        i++;
+   }
+}
